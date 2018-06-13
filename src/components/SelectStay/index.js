befc8b7e1e7276dbceb3de75d0b589e6ae0f9b96
shaileshsingh <shaileshsingh@classic>
2018-06-13 08:11:36 -0400

    update the select city dropdown

--- src/components/SelectStay/index.js
+++ src/components/SelectStay/index.js
@@ -7,10 +7,11 @@ import React from 'react';
 import { FormGroup, Label } from 'reactstrap';
 import images from '../../images';
 import './style/style.scss';
-
-class SelectStay extends React.Component {
-    constructor() {
-        super();
+interface Props {}
+interface State {}
+class SelectStay extends React.Component<Props, State> {
+      constructor(props: Props) {
+        super(props);
         this.state = {
 
             show: false,
@@ -23,6 +24,17 @@ class SelectStay extends React.Component {
             show: !this.state.show,
         });
     }
+    toggleSVal = (e) => {
+        let listElements =this.refs.ddSVal.children;
+        for (var i = 0; i < listElements.length; i++) {
+         listElements[i].classList.remove('selected');
+        }
+        this.refs.ddVal.innerText= e.target.children[0].innerText;
+        e.target.classList.add('selected');
+        this.setState({
+            show: !this.state.show,
+        });
+    }
 
     render() {
         const hide = {
@@ -31,13 +43,13 @@ class SelectStay extends React.Component {
         return (
             <FormGroup className="CustomForm">
                 <Label for="exampleSelect">Staying in</Label>
-                <div role="button" tabIndex={0} onClick={this.toggle} className="DropdownValue">Medina</div>
+                <div role="button" tabIndex={0} onClick={this.toggle} className="DropdownValue" ref="ddVal">Medina</div>
                 <div style={hide} className="Dropdown">
                     <span><img src={images.current_location} alt="LOCATION" />CURRENT LOCATION</span>
-                    <ul>
-                        <li><strong>Makkah</strong>, Saudi Arabia</li>
-                        <li className="selected"><strong>Madinah</strong>, Saudi Arabia</li>
-                        <li><strong>Jeddah</strong>, Saudi Arabia</li>
+                    <ul  ref="ddSVal">
+                        <li className="selected" onClick={this.toggleSVal}><strong>Makkah</strong>, Saudi Arabia</li>
+                        <li  onClick={this.toggleSVal} ><strong>Madinah</strong>, Saudi Arabia</li>
+                        <li onClick={this.toggleSVal}><strong>Jeddah</strong>, Saudi Arabia</li>
                     </ul>
                 </div>
             </FormGroup>
