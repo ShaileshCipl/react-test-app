a2c08732f02cd51861dd1578b5bca6d5d09c98fd
shivani <shivani@classicinformatics.com>
2018-06-13 02:56:41 -0400

    test components  updated

--- src/components/RoomSelection/index.js
+++ src/components/RoomSelection/index.js
@@ -4,11 +4,11 @@
 *
 */
 import React from 'react';
-import {  Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './style/style.scss';
 
 class RoomSelection extends React.Component {
-      constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       modal: false
@@ -22,26 +22,33 @@ class RoomSelection extends React.Component {
       modal: !this.state.modal
     });
   }
-render() {
-  return (
-            <div className="RoomSelection">
-              <div className="RoomSelectionLabel" role="button" tabIndex={0}  onClick={this.toggle}>
-                <p>Passengers</p>
-              </div>  
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                  <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                  <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                  </ModalFooter>
-                </Modal>
+  render() {
+    return (
+      <div className="RoomSelection">
+        <div className="RoomSelectionLabel" role="button" tabIndex={0} onClick={this.toggle}>
+          <p>Passengers</p>
+          <div className="Details">
+            <span className="count">01</span>
+            <span className="RightInfo">
+              <span className="room">Room</span>
+              <span className="passenger">Adult 1</span>
+            </span>
           </div>
-        
-  );
-}
+        </div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+
+    );
+  }
 }
 
 RoomSelection.propTypes = {
